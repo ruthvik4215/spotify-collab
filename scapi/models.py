81c5a366f007a1e67a7ea79f3f219f9d07c6edd2
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-08-07 23:55:03 +0530

    Database setup.

---
+++ scapi/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import random
+import string
+
+def generateUniqueCode():
+    codeLength = 8
+    uniqueString = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+    # use search algorithm for better result and faster performance.
+    while True:
+        code = ''.join(random.choices(uniqueString, k=codeLength))
+        if(Room.objects.filter(code=code).count() == 0):
+            break
+    return code
+
+
+# Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    canGuestPause = models.BooleanField(null=False, default=False)
+    votesToSkip = models.IntegerField(null=False, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
